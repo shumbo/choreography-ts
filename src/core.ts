e58f36e5dd908751517e11eeb753557c055ca283
Shun Kashiwa <shunthedev@gmail.com>
2023-03-31 19:46:21 -0700

    add comments

--- src/core.ts
+++ src/core.ts
@@ -1,12 +1,26 @@
-export type Location = string | number | symbol;
+/**
+ * A type for representing a location
+ */
+export type Location = string;
 
+/**
+ * A class for representing a located value
+ * @typeParam T - The type of the located value
+ * @typeParam L1 - The location of the located value
+ */
 export class Located<T, L1 extends Location> {
+  /**
+   * Create a new located value
+   * @param value value
+   * @param key the key associated with the location
+   */
   constructor(value: T, key: Symbol) {
     this.value = value;
     this.key = key;
   }
   /**
-   *
+   * The internal function to get the normal value.
+   * Use `unwrap` if not defining a custom backend.
    * @internal
    * @param key
    * @returns
@@ -34,6 +48,9 @@ export type Dependencies<L extends Location> = {
 
 /**
  * Perform a local computation at location `L1` and return a value of type `T`
+ * @typeParam L - A set of possible locations
+ * @typeParam L1 - The location of the computation
+ * @typeParam T - The type of the returned value
  */
 export type Locally<L extends Location> = <L1 extends L, T>(
   location: L1,
@@ -68,12 +85,23 @@ export type Call<L extends Location> = <
   args: Args
 ) => Promise<Return>;
 
+/**
+ * A choreography is a function that takes a set of dependencies and a set of arguments and returns a set of results
+ * @typeParam L - A set of possible locations
+ * @typeParam Args - The types of the arguments. Must be an array of located values.
+ * @typeParam Return - The types of the return values. Must be an array of located values.
+ * @param deps - The operators that can be used inside the choreography.
+ * @param args - The arguments of the choreography.
+ */
 export type Choreography<
   L extends Location,
   Args extends Located<any, L>[] = [],
   Return extends Located<any, L>[] = []
-> = (x: Dependencies<L>, args: Args) => Promise<Return>;
+> = (deps: Dependencies<L>, args: Args) => Promise<Return>;
 
+/**
+ * A utility to filter out the values not located at `L1` from an array of located values
+ */
 export type LocatedElements<L extends Location, L1 extends L, A> = A extends [
   Located<infer T, infer L2>,
   ...infer TAIL
@@ -83,7 +111,18 @@ export type LocatedElements<L extends Location, L1 extends L, A> = A extends [
     : [undefined, ...LocatedElements<L, L1, TAIL>]
   : [];
 
+/**
+ * An interface for a backend
+ * @typeParam L - A set of possible locations
+ */
 export interface Backend<L extends Location> {
+  /**
+   * Execute a choreography at a given location
+   * @param choreography - a choreography to execute
+   * @param location - a location to execute the choreography at
+   * @param args - the arguments of the choreography. Pass `undefined` for arguments that are not located at `location`
+   * @returns - the return values of the choreography. Elements of the array are `undefined` if the corresponding return value is not located at `location`
+   */
   run: <
     L1 extends L,
     Args extends Located<L, any>[],

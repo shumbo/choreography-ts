681799cc2d9f54efa5ab166141ab0091d7e8ce6b
Shun Kashiwa <shunthedev@gmail.com>
2023-06-18 13:52:04 -0700

    use commonjs

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -10,18 +10,12 @@ import type { JestConfigWithTsJest } from "ts-jest";
 export const JestConfigBase: JestConfigWithTsJest = {
   // [...]
   maxWorkers: 1,
-  extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
+    "^.+\\.tsx?$": "ts-jest",
   },
 };

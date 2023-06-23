a04c08c98b2fca77cb97acc8679d5e3a74c10e37
Shun Kashiwa <shunthedev@gmail.com>
2023-06-22 17:40:24 -0700

    add ivar

---
+++ packages/core/src/lib/ivar.test.ts
@@ -0,0 +1,34 @@
+import { IVar } from "./ivar";
+
+describe("IVar", () => {
+  test("read before write", async () => {
+    const ivar = new IVar<number>();
+    const readPromise = ivar.read();
+    ivar.write(42);
+    expect(await readPromise).toBe(42);
+  });
+  test("read after write", async () => {
+    const ivar = new IVar<number>();
+    ivar.write(42);
+    expect(await ivar.read()).toBe(42);
+  });
+  test("read twice before write", async () => {
+    const ivar = new IVar<number>();
+    const readPromise1 = ivar.read();
+    const readPromise2 = ivar.read();
+    ivar.write(42);
+    expect(await readPromise1).toBe(42);
+    expect(await readPromise2).toBe(42);
+  });
+  test("read twice after write", async () => {
+    const ivar = new IVar<number>();
+    ivar.write(42);
+    expect(await ivar.read()).toBe(42);
+    expect(await ivar.read()).toBe(42);
+  });
+  test("write twice", async () => {
+    const ivar = new IVar<number>();
+    ivar.write(42);
+    expect(() => ivar.write(42)).toThrow();
+  });
+});

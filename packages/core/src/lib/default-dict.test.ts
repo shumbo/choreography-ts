8f3a6ab6e6e7e2f1230f1827e8f1ae67518d229e
Shun Kashiwa <shunthedev@gmail.com>
2023-06-22 17:40:19 -0700

    add default dict

---
+++ packages/core/src/lib/default-dict.test.ts
@@ -0,0 +1,22 @@
+import { DefaultDict } from "./default-dict";
+
+describe("DefaultDict", () => {
+  test("get before set", () => {
+    const dict = new DefaultDict<string, number>(() => 42);
+    expect(dict.get("foo")).toBe(42);
+  });
+  test("get after set", () => {
+    const dict = new DefaultDict<string, number>(() => 42);
+    dict.set("foo", 43);
+    expect(dict.get("foo")).toBe(43);
+  });
+  test("get twice before set", () => {
+    const dict = new DefaultDict<string, number>(() => 42);
+    expect(dict.get("foo")).toBe(42);
+    expect(dict.get("foo")).toBe(42);
+  });
+  test("falsy default", () => {
+    const dict = new DefaultDict<string, number>(() => 0);
+    expect(dict.get("foo")).toBe(0);
+  });
+});

68b2e295d8bb81408425801a23b7610ced1559f4
Shun Kashiwa <shunthedev@gmail.com>
2023-06-23 08:37:47 -0700

    enable infrastructure on eslint plugin

---
+++ packages/eslint-plugin-choreography-ts/README.md
@@ -0,0 +1,3 @@
+# `@choreography-ts/eslint-plugin-choreography-ts`
+
+This package contains the ESLint plugin for choreography-ts.
---
+++ packages/eslint-plugin-choreography-ts/src/index.test.ts
@@ -0,0 +1,5 @@
+describe("eslint-plugin-choreography-ts", () => {
+  it("should be defined", () => {
+    expect(true).toBe(true);
+  });
+});

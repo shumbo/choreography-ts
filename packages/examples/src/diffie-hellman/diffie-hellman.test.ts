369c5a6199409d33b7f0d178ef6017f9b88d393c
Rohan Kapur <kapur2001@gmail.com>
2023-07-08 10:04:29 -0700

    Diffie-Hillman algorithm and test implemented and passing! Also added README for examples directory. I hope it's all good! :)

---
+++ packages/examples/src/diffie-hellman/diffie-hellman.test.ts
@@ -0,0 +1,18 @@
+import { diffieHellman, Location } from "./diffie-hellman";
+import { ExpressBackend } from "@choreography-ts/backend-express";
+
+// Test the diffie hellman implementation using Jest
+describe("Diffie Hellman", () => {
+  it("should return the same shared key", async () => {
+    const backend = new ExpressBackend<Location>({
+      alice: ["localhost", 3000],
+      bob: ["localhost", 3001],
+    });
+    const keyExchange = diffieHellman("alice", "bob");
+    const [[s1, a], [b, s2]] = await Promise.all([
+      backend.epp(keyExchange, "alice")([false]),
+      backend.epp(keyExchange, "bob")([undefined]),
+    ]);
+    expect(s1).toEqual(s2); // The shared key should be the same
+  });
+});

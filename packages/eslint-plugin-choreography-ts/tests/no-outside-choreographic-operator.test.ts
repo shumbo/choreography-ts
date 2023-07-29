8c5b47d7eec77d3f6e6cf5000fb8d00729a4177d
Rohan Kapur <kapur2001@gmail.com>
2023-07-28 23:55:13 -0700

    Added test case for more code coverage; removed unecessary suggestion check

--- packages/eslint-plugin-choreography-ts/tests/no-outside-choreographic-operator.test.ts
+++ packages/eslint-plugin-choreography-ts/tests/no-outside-choreographic-operator.test.ts
@@ -123,32 +123,8 @@ ruleTester.run("no-outside-choreographic-operator", noOutsideOperatorRule, {
       }`,
       errors: [
         {
+          // suggestion already covered by output
           messageId: "error",
-          suggestions: [
-            {
-              messageId: "suggestion",
-              // Correct line spacing in output
-              output: `
-      const test2: Choreography<Locations> = async ({
-        locally,
-        broadcast,
-        colocally,
-      }) => {
-        const [deliveryDateAtBuyer] = await colocally(
-          ["buyer1", "seller"],
-          async ({ peel }) => {
-            const sharedDecision = peel(decision);
-            if (sharedDecision) {
-              const deliveryDateAtSeller = await locally(
-                "seller",
-                (unwrap) => deliveryDateTable.get(unwrap(titleAtSeller))
-              );
-            }
-          }
-        )
-      }`,
-            },
-          ],
         },
         {
           messageId: "error",
@@ -164,6 +140,79 @@ ruleTester.run("no-outside-choreographic-operator", noOutsideOperatorRule, {
         const [deliveryDateAtBuyer] = await colocally(
           ["buyer1", "seller"],
           async ({ locally }) => {
+            const sharedDecision = peel(decision);
+            if (sharedDecision) {
+              const deliveryDateAtSeller = await locally(
+                "seller",
+                (unwrap) => deliveryDateTable.get(unwrap(titleAtSeller))
+              );
+            }
+          }
+        )
+      }`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "invalid test case 3",
+      code: `
+      const test2: Choreography<Locations> = async ({
+        locally,
+        broadcast,
+        colocally,
+      }) => {
+        const [deliveryDateAtBuyer] = await colocally(
+          ["buyer1", "seller"],
+          async (arg) => {
+            const sharedDecision = peel(decision);
+            if (sharedDecision) {
+              const deliveryDateAtSeller = await locally(
+                "seller",
+                (unwrap) => deliveryDateTable.get(unwrap(titleAtSeller))
+              );
+            }
+          }
+        )
+      }`,
+      output: `
+      const test2: Choreography<Locations> = async ({
+        locally,
+        broadcast,
+        colocally,
+      }) => {
+        const [deliveryDateAtBuyer] = await colocally(
+          ["buyer1", "seller"],
+          async ({ peel }, arg) => {
+            const sharedDecision = peel(decision);
+            if (sharedDecision) {
+              const deliveryDateAtSeller = await locally(
+                "seller",
+                (unwrap) => deliveryDateTable.get(unwrap(titleAtSeller))
+              );
+            }
+          }
+        )
+      }`,
+      errors: [
+        {
+          messageId: "error",
+        },
+        {
+          messageId: "error",
+          suggestions: [
+            {
+              messageId: "suggestion",
+              output: `
+      const test2: Choreography<Locations> = async ({
+        locally,
+        broadcast,
+        colocally,
+      }) => {
+        const [deliveryDateAtBuyer] = await colocally(
+          ["buyer1", "seller"],
+          async ({ locally }, arg) => {
             const sharedDecision = peel(decision);
             if (sharedDecision) {
               const deliveryDateAtSeller = await locally(

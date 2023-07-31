da8a0292a54008cc9f7b21e8a3704c6dcf6cf80c
Rohan Kapur <kapur2001@gmail.com>
2023-07-31 09:11:20 -0700

    `no-renaming-operator` rule modification (#27)
    * `no-renaming-operator` rule modification
    Modified `no-renaming-operator` rule to apply to choreographies as arguments
    Added test case covering this
    Modified linting messages for style conformity
    
    * Add test case
    Add test for choreography as `call` operator argument for `no-renaming-operator` rule

--- packages/eslint-plugin-choreography-ts/tests/no-renaming-operator.test.ts
+++ packages/eslint-plugin-choreography-ts/tests/no-renaming-operator.test.ts
@@ -58,5 +58,49 @@ ruleTester.run("no-renaming-operator", noRenameRule, {
         },
       ],
     },
+    {
+      name: "test for error in choreography as a `colocally` argument",
+      code: `
+      type Locations = "alice" | "bob" | "carol";
+      const _test: Choreography<Locations> = async ({ colocally }) => {
+        await colocally(
+          ["alice", "bob"],
+          async ({ locally: l }) => {
+            await l("alice", () => {
+              console.log("Alice here");
+              return [];
+            });
+            return [];
+          },
+          []
+        );
+        return [];
+      };`,
+      errors: [
+        {
+          messageId: "rename",
+        },
+      ],
+    },
+    {
+      name: "test for error in choreography as a `call` argument",
+      code: `
+      type Locations = "alice" | "bob" | "carol";
+      const _test: Choreography<Locations> = async ({ call }) => {
+        await call(async ({ locally: l }) => {
+          await l("alice", () => {
+            console.log("Alice here");
+            return [];
+          });
+          return [];
+        }, []);
+        return [];
+      };`,
+      errors: [
+        {
+          messageId: "rename",
+        },
+      ],
+    },
   ],
 });

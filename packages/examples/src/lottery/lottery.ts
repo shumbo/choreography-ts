db16c03c274fa973f91ac28c1f5f39a9e49f6c03
psilospore <syedjafri99@gmail.com>
2024-10-10 19:15:01 -0400

    Add serverShares

--- packages/examples/src/lottery/lottery.ts
+++ packages/examples/src/lottery/lottery.ts
@@ -1,4 +1,4 @@
-import { Choreography, Located, Projector, Location} from "@choreography-ts/core";
+import { Choreography, Located, Projector, Location, Faceted} from "@choreography-ts/core";
 import {
   HttpConfig,
   ExpressTransport,
@@ -9,6 +9,7 @@ type FiniteField = number // TODO use some finite field library
 const randomFp = () => Math.random();
 
 // Perhaps I can pass it in as an argument?
+// TODO also test this. Possibly new combinators (fanIn, fanOut, parallel) might have a bug
 export const lottery = <SL extends Location, CL extends Location>(
   serverLocations : SL[],
   clientLocations : CL[]
@@ -19,24 +20,39 @@ export const lottery = <SL extends Location, CL extends Location>(
 > => {
   const c: Choreography<
   "analyst" | SL | CL,
-  [Located<FiniteField, "analyst">]> = async ({ locally, comm, multicast, colocally, call }, []) => {
+  [Located<FiniteField, "analyst">]> = async ({ locally, comm, multicast, enclave, call, fanin, fanout, parallel }, []) => {
 
-      const secret = await locally(clientLocations, (unwrap) => {
+      const secret = await parallel(clientLocations, async (unwrap) => {
         return randomFp();
-      }
+      })
 
-      // TODO What's the analogue of parallel in choreography-ts?
-      const clientShares = await colocally(clientLocations, (unwrap) => {
-        const freeShares = Array.from({ length: n }, () => randomFp());
-        var serverToShares: Record<SL, FiniteField> = {};
+      const clientShares = await parallel(clientLocations, async (_ , unwrap) => {
+        const freeShares = Array.from({ length: serverLocations.length }, () => randomFp());
+        var serverToShares : any = {} // - Record<SL, FiniteField> = {};
         serverLocations.forEach((server, i) => {
-          serverToShares[server] = unwrap(secret) - sum(freeShares);
+          serverToShares[server] = unwrap(secret) - freeShares.reduce((a, b) => a + b, 0);
         });
         return serverToShares;
       })
-    // TODO implement
+
+      // Weird that Q extends is needed here
+      const serverShares : Faceted<[FiniteField], SL> = await fanout(serverLocations, <Server extends SL>(server : Server) => {
+        const c : Choreography<SL | CL | "analyst", [], [Located<[FiniteField], Server>]> = async ({ locally, fanin }) => {
+          // Temporary code to make it compile might not be the type we want
+          // const temp = undefined as any as Located<[FiniteField], Server>
+          // return [temp]
+          const temp : Located<[FiniteField], Server> = await fanin(clientLocations, serverLocations, async (client) => {
+              const c2 : Located<FiniteField, Server> = await locally(client, (unwrap) => {
+                let clientShare = unwrap(clientShares);
+                return clientShare[server];
+              })
+              return c2
+            })
+          return [temp]
+        }
+        return c
+        })
     return 1;
   }
-  // Do we need to do this
   return c;
 }
